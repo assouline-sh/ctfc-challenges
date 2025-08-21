13118a223c97a0af262ab047b62b04a24b4f14df
shan <94480922+assouline-sh@users.noreply.github.com>
2025-08-21 14:23:55 -0700

    Delete dir

--- lil_less_easy/salem_secrets/script.js
+++
@@ -1,7 +0,0 @@
-function setCookie(name, value) {
-    const encodedValue = encodeURIComponent(value);
-    const cookie = `${name}=${encodedValue};`;
-    document.cookie = cookie;
-}
-
-setCookie('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkJsdWVUaWdlcjM0IiwicGFzcyI6ImNHRnpjeTUwZUhRSyIsImlhdCI6MTY5MzA5NDQwMH0.49imX4EZCIepFTJ9pHptOwH53b-7WhGSBHc_4LcAyTA');
--- lil_less_easy/secret_web/README.md
+++
@@ -1,9 +0,0 @@
-# Secret Web
-## Web
-
-- Look at source code, see css file in location /secret/assets
-- Manipulate the URL to navigate to /secret, see location /supersecret in source code
-- Navigate to /supersecret, and page hints that you need to make it alert
-- Submit <script>alert();</script> to the page, get the flag
-
-CTF{traversing_new_paths}
